1eb8bf9ea5b8eb29fc940841b825e39f3dd058a9
gautric <gautric@redhat.com>
2022-03-11 11:56:57 +0100

    update main function

--- src/helloworld-main.c
+++ src/helloworld-main.c
@@ -1,7 +1,14 @@
 #include "helloworld-lib.h"
 
-int main() {
-    int ret = helloworld("Thomas");
+int main(int argc, char* argv[]) {
+    int ret = 0;
+
+    if(argc == 2 ) {
+        ret = helloworld(argv[1]);
+    } else {
+        ret = helloworld("Thomas");    
+    }
+
     printf("ret = %d (from helloworld)\n", ret);
     return(0);
 }
